1fcf1fd877838d62bc0e3e66d3138603d8d34008
@vasga-floze <gabriela.floze@gmail.com>
2021-03-15 23:05:34 -0600

    se agregan las paginas de ejercicios, que se llaman desde el menu de navegacion

---
+++ ejercicio5.php
@@ -0,0 +1,51 @@
+<?php
+
+    //incluir archivo de cabecera
+    include "header.php"
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 2</title>
+</head>
+<body>
+    <!-- DESARROLLO DEL EJERCICIO -->
+
+        <?php
+
+        ?>
+
+    <!-- //FIN DEL EJERCICIO -->
+
+    <!-- footer -->
+<div class="footer">
+		<div class="container">
+			<div class="footer-grids">
+				<div class="col-md-3 footer-nav wow fadeInLeft animated" data-wow-delay=".5s">
+					<h4>Navegacion</h4>
+					<ul>
+					<li>
+						<li><a href="ejercicio1.php">Ejercicio 1</a></li>
+						<li><a href="ejercicio2.php">Ejercicio 2</a></li>
+						<li><a href="ejercicio3.php">Ejercicio 3</a></li>
+						<li><a href="ejercicio4.php">Ejercicio 4</a></li>
+                        <li><a href="ejercicio5.php">Ejercicio 5</a></li>
+                        <li><a href="ejercicio6.php">Ejercicio 6</a></li>
+                        <li><a href="ejercicio7.php">Ejercicio 7</a></li>
+								
+					</ul>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+			<div class="copyright wow fadeInUp animated" data-wow-delay=".5s">
+				<p>© 2021  . Universidad Gerardo Barrios</p>
+			</div>
+		</div>
+	</div>
+	<!-- //footer -->
+</body>
+</html>
