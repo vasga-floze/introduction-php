102526cd65f3c3d6ec5be07c15783a6e206ad996
@vasga-floze <gabriela.floze@gmail.com>
2021-03-16 00:31:14 -0600

    se elimina el footer para tener una vista mas limpia

--- ejercicio2.php
+++ ejercicio2.php
@@ -21,33 +21,9 @@
         ?>
 
     <!-- //FIN DEL EJERCICIO -->
-
-    <!-- footer -->
-<div class="footer">
-		<div class="container">
-			<div class="footer-grids">
-				<div class="col-md-3 footer-nav wow fadeInLeft animated" data-wow-delay=".5s">
-					<h4>Navegacion</h4>
-					<ul>
-					<li>
-						<li><a href="ejercicio1.php">Ejercicio 1</a></li>
-						<li><a href="ejercicio2.php">Ejercicio 2</a></li>
-						<li><a href="ejercicio3.php">Ejercicio 3</a></li>
-						<li><a href="ejercicio4.php">Ejercicio 4</a></li>
-                        <li><a href="ejercicio5.php">Ejercicio 5</a></li>
-                        <li><a href="ejercicio6.php">Ejercicio 6</a></li>
-                        <li><a href="ejercicio7.php">Ejercicio 7</a></li>
-								
-					</ul>
-				</div>
-				<div class="clearfix"> </div>
-			</div>
-			<div class="copyright wow fadeInUp animated" data-wow-delay=".5s">
-				<p>© 2021  . Universidad Gerardo Barrios</p>
-			</div>
-		</div>
+    <div class="copyright wow fadeInUp animated" data-wow-delay=".5s">
+		<p>© 2021  . Universidad Gerardo Barrios</p>
 	</div>
-	<!-- //footer -->
 </body>
 </html>
 
