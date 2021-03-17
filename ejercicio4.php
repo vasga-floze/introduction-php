adb6f0686df516b9e6060e07ca1cfc9d64744a85
@vasga-floze <gabriela.floze@gmail.com>
2021-03-17 00:52:13 -0600

    agrega indicacion para ejercicio cuatro

--- ejercicio4.php
+++ ejercicio4.php
@@ -15,9 +15,28 @@
 <body>
     <!-- DESARROLLO DEL EJERCICIO -->
 
-        <?php
+    <div class="team" body style="background-color:#2DCB74;">
+        <div class="container">
+                <div class="popular-heading team-heading">
+                    <h3 class="wow fadeInUp animated" data-wow-delay=".5s">Ejercicio 4</h3>
+                    <h2 class="wow fadeInUp animated" data-wow-delay=".5s" style="color:#ffffff;"> Cree un array asociativo en donde almacene los datos mostrados en la siguiente tabla.<br/></h2>
+                </div>
+                <div class=" team-heading">
+                    <img src="images/cuatro.jpg" class="img-thumbnail wow fadeInRight animated" alt="100px" >
+                </div>
+                <div class="team-heading">
+                    <br/><br/>
+                    <h2 class="wow fadeInUp animated team-heading" data-wow-delay=".5s" style="color:#ffffff; font-family: 'Francois One', sans-serif">Se pide:</h2>
+                    <h3 class="wow fadeInUp animated" data-wow-delay=".5s" style="color:#ffffff; font-family: 'Francois One', sans-serif">- Mostrar los datos del array en una tabla</h3>
+                    <h3 class="wow fadeInUp animated" data-wow-delay=".5s" style="color:#ffffff; font-family: 'Francois One', sans-serif">- Calcular el salario promedio</h3>
+                    <h3 class="wow fadeInUp animated" data-wow-delay=".5s" style="color:#ffffff; font-family: 'Francois One', sans-serif">- Cantidad de empleados por departamento</h3>
+                </div>
+        </div>
+    </div>
 
-        ?>
+    <?php
+
+    ?>
 
     <!-- //FIN DEL EJERCICIO -->
 
