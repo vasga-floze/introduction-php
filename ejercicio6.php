ee0de7190f31cda19f54535700b415da83f3a2a7
david <david1098cr@hotmail.com>
2021-03-19 16:19:14 -0600

    agrega ejercicio6

--- ejercicio6.php
+++ ejercicio6.php
@@ -15,7 +15,52 @@
 <body>
     <!-- DESARROLLO DEL EJERCICIO -->
 
+	<center> 
+    
+	<P>Cree una función que calcule cual es el número mayor de dos enteros,<BR>
+	 establezca el valorde salida correspondiente y haga uso de strict_types.</P>
+	
+	 <hr >
+	
+	
+		<FORM ACTION="ejercicio6.php"  METHOD="POST"> 
+	
+			Introduzca el primer número:
+			<INPUT  TYPE="text" NAME="numero1"><BR> 
+			Introduzca el segundo número:
+			 <INPUT TYPE="text" NAME="numero2"><BR> 
+			<INPUT TYPE="submit" VALUE="Compara" NAME="btn1" class="btn btn-success"> 
+	
+		</FORM>   
+		
+	</center>
+
+
         <?php
+		 if(isset($_POST['btn1'])){
+
+			$num1 = $_POST['numero1'];
+			$num2 = $_POST['numero2'];
+			
+			 comparar($num1,$num2);
+			
+			}
+			
+			
+			function comparar ($v1,$v2) {
+			
+			if ($v1>$v2){
+				 echo "El número mayor es (".$v1.")";
+			}
+			elseif ($v1==$v2){
+				echo "El número (".$v1.") es igual al número (".$v2.")";
+			}
+			else{
+				echo "El primer número mayor es(".$v2.")";
+			}
+			
+			}
+		
 
         ?>
 
