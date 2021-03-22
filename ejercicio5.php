4779f875a1c7c0edadcaa15998ef79b707ec955e
@vasga-floze <gabriela.floze@gmail.com>
2021-03-21 21:37:59 -0600

    agrega enunciado del ejercicio5 y agrega animación a footer

--- ejercicio5.php
+++ ejercicio5.php
@@ -14,15 +14,22 @@
 </head>
 <body>
     <!-- DESARROLLO DEL EJERCICIO -->
-
-        <?php
-
-        ?>
-
+    <!-- Se agrega un titulo y la descripcion del ejercicio -->
+    <div class="team" style="background-color:#2DCB74;">
+        <div class="container">
+            <div class="popular-heading team-heading">
+            <h3 class="wow fadeInUp animated" data-wow-delay=".5s">Ejercicio 5</h3>
+            <h2 class="wow fadeInUp animated" data-wow-delay=".5s" style="color:#ffffff;">Escriba una función que reciba como argumentos n cantidad de números, 
+            la salida deberá mostrar la suma de los números.</h2>
+            </div>
+        </div>
+    </div>
+    <br>
+    
     <!-- //FIN DEL EJERCICIO -->
 
-    <div class="copyright wow fadeInUp animated" data-wow-delay=".5s">
-		<p>© 2021  . Universidad Gerardo Barrios</p>
+    <div class="copyright wow fadeInUp animated" data-wow-delay=".5s" body style="background-color:#2DCB74;">
+		<p class= "text-center">© 2021  . Universidad Gerardo Barrios</p>
 	</div>
 </body>
 </html>
