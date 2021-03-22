4c457ada801996838fa54bc979690698d60c0f22
@vasga-floze <gabriela.floze@gmail.com>
2021-03-22 00:23:58 -0600

    actualiza diseño y aumenta tamaño de fuente

--- ejercicio2.php
+++ ejercicio2.php
@@ -14,13 +14,13 @@
     <title>Ejercicio 2</title>
 </head>
 <body>
-<div class="team">
+<div class="team" body style="background-color:#2DCB74;">
       <div class="container">
         <div class="popular-heading team-heading">
           <h3 class="wow fadeInUp animated" data-wow-delay=".5s">Ejercicio 2</h3>
-          <p class="wow fadeInUp animated" data-wow-delay=".5s">Realice un script PHP que muestre mediante la utilización de variables 
+          <h2 class="wow fadeInUp animated" data-wow-delay=".5s" style="color:#ffffff;">Realice un script PHP que muestre mediante la utilización de variables 
           sus datos personales: nombre  completo,  lugar de  nacimiento  (departamento y  país, si  es extranjero), edad  y Código de Estudiante. 
-          Muestre estos datos en una tabla.</p>    
+          Muestre estos datos en una tabla.</h2>    
            </div>
                 </div>
             </div>
@@ -28,15 +28,15 @@
 <br>
     <!-- DESARROLLO DEL EJERCICIO -->
 
-    <table class="table table-bordered table-dark wow fadeInRight animated">>
+    <table class="table table-bordered table-dark wow fadeInRight animated">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">Nombre</th>
-      <th scope="col">Pais</th>
-      <th scope="col">Departamento de nacimiento</th>
-	  <th scope="col">Edad</th>
-	  <th scope="col">Codigo de estudiante</th>
+      <th scope="col"><h4>#</h4></th>
+      <th scope="col"><h4>Nombre</h4></th>
+      <th scope="col"><h4>País</h4></th>
+      <th scope="col"><h4>Departamento de nacimiento</h4></th>
+	  <th scope="col"><h4>Edad</h4></th>
+	  <th scope="col"><h4>Código de estudiante</h4></th>
 	  
     </tr>
   </thead>
@@ -44,44 +44,44 @@
   <tbody>
     <tr>
       <th scope="row">1</th>
-      <td> <?php $nombre="Roberto Kilmar Chevez Reyes";
-	  echo $nombre; ?> </td>
-	  <td> <?php $pais="";
-	  echo $pais; ?> </td>
-      <td> <?php $nacimiento="Usulutan";
-	  echo $nacimiento; ?> </td>
-	   <td> <?php $edad="21";
-	  echo $edad; ?> </td>
-	  <td> <?php $codigo="SMIS619019";
-	  echo $codigo; ?> </td>
+      <td> <h4><?php $nombre="Roberto Kilmar Chevez Reyes";
+	  echo $nombre; ?></h4> </td>
+	  <td> <h4><?php $pais="El Salvador";
+	  echo $pais; ?></h4> </td>
+      <td> <h4><?php $nacimiento="Usulutan";
+	  echo $nacimiento; ?></h4> </td>
+	   <td> <h4><?php $edad="21";
+	  echo $edad; ?></h4> </td>
+	  <td> <h4><?php $codigo="SMIS619019";
+	  echo $codigo; ?></h4> </td>
       </thead>
 	  </tr>
 
     <tr>
     <th scope="row">2</th>
-      <td> <?php $nombre="Vasti Gabriela Flores Zelaya";
-	  echo $nombre; ?> </td>
-	  <td> <?php $pais="Guatemala";
-	  echo $pais; ?> </td>
-      <td> <?php $nacimiento="Antigua Guatemala";
-	  echo $nacimiento; ?> </td>
-	   <td> <?php $edad="21";
-	  echo $edad; ?> </td>
-	  <td> <?php $codigo="SMIS023453";
-	  echo $codigo; ?> </td>
+      <td> <h4><?php $nombre="Vasti Gabriela Flores Zelaya";
+	  echo $nombre; ?></h4> </td>
+	  <td> <h4><?php $pais="El Salvador";
+	  echo $pais; ?></h4> </td>
+      <td> <h4><?php $nacimiento="San Miguel";
+	  echo $nacimiento; ?></h4> </td>
+	   <td> <h4><?php $edad="20";
+	  echo $edad; ?></h4> </td>
+	  <td> <h4><?php $codigo="SMIS003119";
+	  echo $codigo; ?></h4> </td>
     </tr>
 
     <th scope="row">3</th>
-      <td> <?php $nombre="David Alexander Cruz Rivas";
-	  echo $nombre; ?> </td>
-	  <td> <?php $pais="";
-	  echo $pais; ?> </td>
-      <td> <?php $nacimiento="San Miguel";
-	  echo $nacimiento; ?> </td>
-	   <td> <?php $edad="22";
-	  echo $edad; ?> </td>
-	  <td> <?php $codigo="SMIS008921";
-	  echo $codigo; ?> </td>
+      <td> <h4><?php $nombre="David Alexander Cruz Rivas";
+	  echo $nombre; ?> </h4></td>
+	  <td> <h4><?php $pais="El Salvador";
+	  echo $pais; ?></h4> </td>
+      <td> <h4><?php $nacimiento="San Miguel";
+	  echo $nacimiento; ?></h4> </td>
+	   <td> <h4><?php $edad="22";
+	  echo $edad; ?></h4> </td>
+	  <td> <h4><?php $codigo="SMIS008921";
+	  echo $codigo; ?></h4> </td>
     </tr>
  
   </tbody>
