c852ba0a88d89429851f55535dd0db7ec5a20c35
@vasga-floze <gabriela.floze@gmail.com>
2021-03-22 00:55:00 -0600

    Solventa error generado por mala administración de pull request xD

--- ejercicio6.php
+++ ejercicio6.php
@@ -17,30 +17,6 @@
 </head>
 <body>
     <!-- DESARROLLO DEL EJERCICIO -->
-    <form method="post" action="ejercicio1.php" class="text-center">
-    <div class="form-group">
-    <label for="num1">Ingrese un número</label><br>
-    <input type="text" name="num1"/>
-	</div>
-
-	
-    <?php 
-    declare(strict_types=1);
-    
-
-   
-    function calcular($numero1, $numero2):float{
-        $numero1=3.4;
-        $numero2=6.4;
-        
-    if ($numero1>$numero2){
-        echo "$numero1 es mayor que $numero2";
-    }
-    else{
-        echo "$numero2 es mayor que $numero1";
-    }
-
-
 	<div class="team" body style="background-color:#2DCB74;">
         <div class="container">
                 <div class="popular-heading team-heading">
@@ -95,6 +71,7 @@
         ?>
 
     <!-- //FIN DEL EJERCICIO -->
+
 	<div class="copyright wow fadeInUp animated" data-wow-delay=".5s" body style="background-color:#2DCB74;">
 		<p class= "text-center">© 2021  . Universidad Gerardo Barrios</p>
 	</div>
