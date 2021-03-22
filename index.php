aaa357ad9ee20df35536f15e1a0f4be7df3a7216
Gisela <gys.espinoza@gmail.com>
2021-03-22 12:53:15 -0600

    Se limpio  rama

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    //incluir archivo de cabecera
-    include "header.php";
-
-?>
-<div class="copyright wow fadeInUp animated" data-wow-delay=".5s">
-	<p>© 2021  . Universidad Gerardo Barrios</p>
-</div>
